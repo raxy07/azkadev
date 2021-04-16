810358e0ed6ad4ef6e788dbbf4af96dd077f7b3a
insaneazka <82513502+insaneazka@users.noreply.github.com>
2021-04-16 16:43:39 +0700

    Update telegram-script-google.js

--- insane-library-info/telegram-script-google.js
+++ insane-library-info/telegram-script-google.js
@@ -5,14 +5,14 @@
     "video": "https://github.com/insaneazka/insaneazka/raw/main/insaneazka.jpg?raw=true",
     "voice": "https://github.com/insaneazka/insaneazka/raw/main/insaneazka.jpg?raw=true",
     "keyboard" :[
-                        [
-                            { "text": "️Donate Me", "url": "https://github.com/insaneazka" }
-                        ],
-                       [
-                            { "text": "️Github", "url": "https://github.com/insaneazka" },
-                            { "text": "️Telegram", "url": "https://www.tiktok.com/" },
-                            { "text": "️YOUTUBE", "url": "https://www.youtube.com/" }
-                        ]
+        [
+            { "text": "️Donate Me", "url": "https://github.com/insaneazka" }
+        ],
+        [
+            { "text": "️Github", "url": "https://github.com/insaneazka" },
+            { "text": "️Telegram", "url": "https://www.tiktok.com/" },
+            { "text": "️YOUTUBE", "url": "https://www.youtube.com/" }
+        ]
     ],
     "caption": "INFORMATION BOT TELEGRAM + LIBRARY\n\n------------------------------------------------------------------------------------------------------\n\nBot Version Info\n\nCcode Name   : @insaneazka\nVersion : 1 stable\nDatabase     : sql google sheet\nLanguage Code : JAVASCRIPT\nCreator Library : @insaneazka\n\n------------------------------------------------------------------------------------------------------"
 }
