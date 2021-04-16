39c673c33af14f360626a32388844c7d2027ea3a
insaneazka <82513502+insaneazka@users.noreply.github.com>
2021-04-16 16:16:46 +0700

    Add files via upload

---
+++ insane-library-info/telegram-script-google.js
@@ -0,0 +1,8 @@
+{
+    "status": "photo",
+    "audio": "https://github.com/insaneazka/insaneazka/raw/main/insaneazka.jpg?raw=true",
+    "photo": "https://github.com/insaneazka/insaneazka/raw/main/insaneazka.jpg?raw=true",
+    "video": "https://github.com/insaneazka/insaneazka/raw/main/insaneazka.jpg?raw=true",
+    "voice": "https://github.com/insaneazka/insaneazka/raw/main/insaneazka.jpg?raw=true",
+    "caption": "INFORMATION BOT TELEGRAM + LIBRARY\n\n------------------------------------------------------------------------------------------------------\n\nBot Version Info\n\nCcode Name   : @insaneazka\nVersion : 1 stable\nDatabase     : sql google sheet\nLanguage Code : JAVASCRIPT\nCreator Library : @insaneazka\n\n------------------------------------------------------------------------------------------------------"
+}
