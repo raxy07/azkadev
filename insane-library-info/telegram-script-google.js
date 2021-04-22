4af4b3ccb17807416fc93b4d251b974612321392
insaneazka <82513502+insaneazka@users.noreply.github.com>
2021-04-22 15:34:46 +0700

    Update telegram-script-google.js

--- insane-library-info/telegram-script-google.js
+++ insane-library-info/telegram-script-google.js
@@ -10,9 +10,9 @@
         ],
         [
             { "text": "️Github", "url": "https://github.com/insaneazka" },
-            { "text": "️Telegram", "url": "https://www.tiktok.com/" },
+            { "text": "️Telegram", "url": "t.me/insaneazka" },
             { "text": "️YOUTUBE", "url": "https://www.youtube.com/" }
         ]
     ],
-    "caption": "INFORMATION BOT TELEGRAM + LIBRARY\n\n------------------------------------------------------------------------------------------------------\n\nBot Version Info\n\nCode Name   : Insane Azka\nVersion : 1 stable\nDatabase     : sql google sheet\nLanguage Code : JAVASCRIPT\nSupport My Channel : @insaneazka\nif bot Problem you can report to\nDeveloper : @fuadazka\n\n------------------------------------------------------------------------------------------------------"
+    "caption": "INFORMATION BOT TELEGRAM + LIBRARY\n\n------------------------------------------------------------------------------------------------------\n\nBot Version Info\n\nCode Name   : Insane Azka\nVersion : 1 stable\nDatabase     : sql google sheet\nLanguage Code : JAVASCRIPT\nSupport My Channel : @insaneazka\nDeveloper : @fuadazkainsane / @fuadazka ( cuma ada di telegram )\n\n------------------------------------------------------------------------------------------------------"
 }
