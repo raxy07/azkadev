58239103bb7368189b96123edac6cbca22af4467
insaneazka <82513502+insaneazka@users.noreply.github.com>
2021-04-22 15:37:31 +0700

    Update telegram-script-google.js

--- insane-library-info/telegram-script-google.js
+++ insane-library-info/telegram-script-google.js
@@ -14,5 +14,5 @@
             { "text": "️YOUTUBE", "url": "https://www.youtube.com/" }
         ]
     ],
-    "caption": "INFORMATION BOT TELEGRAM + LIBRARY\n\n------------------------------------------------------------------------------------------------------\n\nBot Version Info\n\nCode Name   : Insane Azka\nVersion : 1 stable\nDatabase     : sql google sheet\nLanguage Code : JAVASCRIPT\nSupport My Channel : @insaneazka\nDeveloper : @fuadazkainsane / @fuadazka ( cuma ada di telegram )\n\n------------------------------------------------------------------------------------------------------"
+    "caption": "INFORMATION BOT TELEGRAM + LIBRARY\n\n------------------------------------------------------------------------------------------------------\n\nBot Version Info\n\nCode Name   : Insane Azka\nVersion : 1 stable\nDatabase     : sql google sheet\nLanguage Code : JAVASCRIPT\nDeveloper : @fuadazkainsane / @fuadazka ( cuma ada di telegram )\n\nPantun: Pergi ke jepang beli kerupuk...Cakep\nSampai sana beli kerupuk:V\nbuat apa pergi jauh ke jepang..\nKalo cuma beli kerupuk\n\n------------------------------------------------------------------------------------------------------"
 }
